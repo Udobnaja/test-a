f5d71a356c2ec5711755cf83f6d2e9737e02b741
udobnaja <udobnaja@gmail.com>
2018-04-07 17:25:46 +0300

    bubble sort

---
+++ 3/index.js
@@ -0,0 +1,20 @@
+(function () {
+    let arr = [7, 6, 3, 0, 2, 1, 5];
+
+    function sort(arr) {
+        let isSort = false;
+
+        for (let i = 0; i < arr.length; i++){
+            if (i !== arr.length - 1){
+                if (arr[i + 1] < arr[i]){
+                    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                    isSort = true;
+                }
+            }
+        }
+
+        return (isSort === true) ? sort(arr) : arr;
+    }
+
+    let g = sort(arr);
+}());
